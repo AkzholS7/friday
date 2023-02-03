1484254a2f90d12636a8aa6f4c856cbd52248149
Akzhol <suborovakzhol7@gmail.com>
2023-02-03 11:20:13 +0600

    fourth commit

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_virtual_machine" "main" {
   name                  = "Akzhols_machine_released_version"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
+  network_interface_ids = [azurerm_network_interface.example.id]
   vm_size               = "Standard_Bs1"
 
   storage_image_reference {
